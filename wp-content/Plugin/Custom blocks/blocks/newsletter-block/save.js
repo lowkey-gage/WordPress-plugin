a88d22769a4bfcd42150b8efe8e278fdf879ed7d
lowkey-gage <156824075+lowkey-gage@users.noreply.github.com>
2025-09-19 19:41:19 -0500

    Add files via upload

---
+++ wp-content/Plugin/Custom blocks/blocks/newsletter-block/save.js
@@ -0,0 +1,21 @@
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useState } from '@wordpress/element';
+
+export default function save({ attributes }) {
+    const { heading, imageUrl, imageDesc, mainText, expandedText } = attributes;
+
+    return (
+        <div {...useBlockProps.save()}>
+            {heading && <RichText.Content tagName="h2" value={heading} />}
+            {imageUrl && <img src={imageUrl} alt={imageDesc} />}
+            {imageDesc && <p className="source">{imageDesc}</p>}
+            {mainText && <RichText.Content tagName="p" value={mainText} />}
+            {expandedText && (
+                <div className="expanded-text" style={{ display: 'none' }}>
+                    <RichText.Content tagName="p" value={expandedText} />
+                </div>
+            )}
+            <button className="toggle-button" type="button">Read More</button>
+        </div>
+    );
+}
