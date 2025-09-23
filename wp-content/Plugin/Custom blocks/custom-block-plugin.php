d7632f7131feca0900373bbfc1f34b931ffd5091
lowkey-gage <156824075+lowkey-gage@users.noreply.github.com>
2025-09-23 09:15:46 -0500

    Add files via upload

---
+++ wp-content/Plugin/Custom blocks/custom-block-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Custom Block Plugin
+ * Description: Adds custom blocks to the Gutenberg editor that you wouldn't typically find in WordPress.
+ * Version: 1.0.0
+ * Author: Gage W. Reed
+ * License: GPLv2
+ */
+
+
+/*
+* Register all blocks in the /blocks/ folder.
+*/
+function cbp_register_blocks() {
+    if ( function_exists( 'register_block_type' ) ) {
+    register_block_type( __DIR__ . '/blocks/newsletter-block' );
+}}
+add_action( 'init', 'cbp_register_blocks' );
+ 
