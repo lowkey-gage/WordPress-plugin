a39042f7afaa0ed792c7e846b8d1cbefc96bc2ae
lowkey-gage <gagewilliamreed@gmail.com>
2025-10-09 20:47:42 -0500

    Overwrite repo with new folder from USB

---
+++ blocks/newsletter-block/index.asset.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Asset file for the newsletter-block fallback script.
+ * This tells WordPress which editor packages to load before this script.
+ */
+return array(
+    'dependencies' => array( 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-block-editor', 'wp-components' ),
+    'version'      => '1.0.0',
+);
